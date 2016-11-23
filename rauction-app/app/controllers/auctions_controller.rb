14e7bf2cab2be7d6ffdfdb558a73fb1e16295a14
Vanessa Muller <vane9090@gmail.com>
2016-11-22 21:31:33 -0500

    modified auctions,dealers, users controllers

--- rauction-app/app/controllers/auctions_controller.rb
+++ rauction-app/app/controllers/auctions_controller.rb
@@ -1,2 +1,37 @@
 class AuctionsController < ApplicationController
+
+  def index
+    @auction = Auction.all
+  end
+
+  def new
+    @auction = Auction.new
+  end
+
+  def create
+    @auction.new(auction_params)
+    if @auction.save
+      # session[:dealer_id] = dealer.id
+      redirect_to @auction
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @auction = Auction.find(params[:id])
+  end
+
+  def destroy
+    @auction = Auction.find(params[:id])
+    @auction.destroy
+
+    redirect_to auctions_path
+  end
+
+  private
+  def auction_params
+    params.require(:auction).permit(:max_price, :title, :year, :make, :model, :miles_from, :address, :color, :features, :expiration_date)
+  end
+
 end
--- rauction-app/app/controllers/dealers_controller.rb
+++ rauction-app/app/controllers/dealers_controller.rb
@@ -1,2 +1,45 @@
 class DealersController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @dealer = Dealer.new
+  end
+
+  def create
+    @dealer = Dealer.new(dealer_params)
+     if @dealer.save
+       session[:dealer_id] = dealer.id
+       redirect_to @dealer
+     else
+       redirect_to '/signup'
+     end
+  end
+
+  def update
+    @dealer = Dealer.find(params[:id])
+
+      if @dealer.update(dealer_params)
+        redirect_to @dealer
+      else
+        render 'edit'
+      end
+  end
+
+  def show
+    @dealer= Dealer.find(params[:id])
+  end
+
+  def destroy
+    @dealer = Dealer.find(params[:id])
+    @dealer.destroy
+
+    redirect_to dealers_path
+  end
+
+  private
+  def dealer_params
+    params.require(:dealer).permit(:company_name, :dealer_name, :address, :email, :phone, :photo)
+  end
 end
