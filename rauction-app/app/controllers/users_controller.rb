880602e28ef8c9677771565b3d8a701cab1a4614
xrey007 <reinaldo.leal001@gmail.com>
2016-11-25 20:48:36 -0500

    adding users.scss users_controller.rb and index.html.erb changes

--- rauction-app/app/controllers/users_controller.rb
+++ rauction-app/app/controllers/users_controller.rb
@@ -11,8 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
      if @user.save
-       session[:user_id] = user.id
-       redirect_to @user
+       session[:user_id] = @user.id
+       redirect_to '/'
      else
        redirect_to '/signup'
      end
