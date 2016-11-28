5681579ad0ca6e25d37813aa6bc4b8fc7508ff0a
xrey007 <reinaldo.leal001@gmail.com>
2016-11-27 20:48:25 -0500

    adding auction create/index page for users

--- rauction-app/config/routes.rb
+++ rauction-app/config/routes.rb
@@ -9,15 +9,20 @@ Rails.application.routes.draw do
   # get 'users/index'
   #
   # get 'users/new'
-resources :users
+  resources :users
 
-root "users#index"
-get '/signup' => 'users#new'
-post '/users' => 'users#create'
+  resources :users do
+    resources :auctions
+  end
 
-get '/login' => 'sessions#new'
-post '/login' => 'sessions#create'
-get '/logout' => 'sessions#destroy'
+  post '/auctions' => 'auctions#create'
+  root "users#index"
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
 
 end
