417fb5af6de6c36e5911d07d835c6cddb9e18f6c
xrey007 <reinaldo.leal001@gmail.com>
2016-11-30 15:19:48 -0500

    adding fixes to the navbar

--- rauction-app/app/test.rb
+++ rauction-app/app/test.rb
@@ -5,3 +5,6 @@ require 'nokogiri'
 @makes = HTTParty.get("http://www.carqueryapi.com/api/0.3/?cmd=getMakes&year=2010")
 
 puts @makes
+
+
+https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=25.6852342,-80.4175613&destinations=25.7583541,-80.3440679&key=AIzaSyAbs34ta5k34QdfBQUf-6nR0x5I5GMnWqY
