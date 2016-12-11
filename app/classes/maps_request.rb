ff5eb242a898642ccf4e9724c0e87c8a62dfa5bb
xrey007 <reinaldo.leal001@gmail.com>
2016-12-11 18:43:26 -0500

    adding env variable for google maps api key to keep it secret

--- app/classes/maps_request.rb
+++ app/classes/maps_request.rb
@@ -2,11 +2,12 @@ require 'httparty'
 require 'nokogiri'
 # THE CLASS BELOW GETS THE HTTPARTY JSON RESPONSE AND PARSES IT TO GET THE MILES IN BETWEEN
 # TWO DESTINATIONS USING THE DESTINATIONS LATITUDE AND LONGITUDE
+GOOGLE_MAPS_MATRIX_API_KEY = ENV["GOOGLE_MAPS_MATRIX_API_KEY"]
 class MapsRequest
   attr_accessor :distance
   def initialize(orgLatitude, orgLongitude, destLatitude, destLongitude)
     @distance = HTTParty.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{orgLatitude},\
-                #{orgLongitude}&destinations=#{destLatitude},#{destLongitude}&key=AIzaSyAbs34ta5k34QdfBQUf-6nR0x5I5GMnWqY")\
+                #{orgLongitude}&destinations=#{destLatitude},#{destLongitude}&key=#{GOOGLE_MAPS_MATRIX_API_KEY}")\
                 ['rows'].first['elements'].first['distance']['text']
   end
 end
