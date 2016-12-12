d61269855dab899a6c11fbcce5b17468efd4ee88
xrey007 <reinaldo.leal001@gmail.com>
2016-12-11 19:25:11 -0500

    trying to still fix that bug

--- app/classes/maps_request.rb
+++ app/classes/maps_request.rb
@@ -6,7 +6,7 @@ class MapsRequest
   attr_accessor :distance
   def initialize(orgLatitude, orgLongitude, destLatitude, destLongitude)
     @distance = HTTParty.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{orgLatitude},\
-                #{orgLongitude}&destinations=#{destLatitude},#{destLongitude}&key=#{ENV['GOOGLE_MAPS_MATRIX_API_KEY']}")\
+                #{orgLongitude}&destinations=#{destLatitude},#{destLongitude}&key=AIzaSyAbs34ta5k34QdfBQUf-6nR0x5I5GMnWqY")\
                 ['rows'].first['elements'].first['distance']['text']
   end
 end
